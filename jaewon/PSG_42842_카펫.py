59cb5daeac12311951063ab13a8c2bc1c7cb3622
jaewon4 <kimjaewon0901@gmail.com>
2023-04-01 10:43:08 +0900

    [PGS] 카펫 / Level2 / 42842 / 김재원

--- jaewon/PSG_42842_카펫.py
+++ jaewon/PSG_42842_카펫.py
@@ -1,10 +1,6 @@
-import sys
-input = sys.stdin.readline
-
 def solution(brown, yellow):
     answer = []
-    a = 0
-    b = 0
+    # yellow가 하나일때는 무조건 brown이 9이다.
     if yellow == 1:
         answer.append(3)
         answer.append(3)
@@ -12,18 +8,12 @@ def solution(brown, yellow):
 
     for j in range(1, yellow):
         if yellow % j == 0:
-            a = j
-            b = yellow // j
-            w = a * 2
-            h = b * 2
-            if brown == w + h + 4:
-                if b+2 in answer:
-                    break
-                elif a+2 in answer:
-                    break
-                else:
-                    answer.append(b+2)
-                    answer.append(a+2)
+            yellow_h = j
+            yellow_w = yellow // j
+            brown_h = yellow_h + 2
+            brown_w = yellow_w + 2
+            # brown의 개수가 같고 answer가 비어있는경우만 => [8,6], [6,8] 들어오는걸 방지
+            if brown == (brown_h - 2) * 2 + brown_w * 2 and len(answer) == 0:
+                answer.append(brown_w)
+                answer.append(brown_h)
     return answer
-
-print(solution(8, 1))
