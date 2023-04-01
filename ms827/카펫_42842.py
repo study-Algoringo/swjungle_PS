5107352a9a6dacb28735563e888c17f99e375389
ms827 <86936388+ms827@users.noreply.github.com>
2023-04-01 10:48:16 +0900

    day1

--- ms827/카펫_42842.py
+++ ms827/카펫_42842.py
@@ -1,9 +1,12 @@
 def solution(brown, yellow):
-    a = 1
-    b = 0
-    while a*b != brown+yellow:
-        b = int(brown //2) - a
-        if a*b == (brown+yellow):
+    a = 1 #세로
+    b = 0 #가로
+    
+    # 갈색 격자 = 2a+2b-4 = a+b-2
+    # 노란색 격자 = (a-2)(b-2)
+    while True:
+        b = int(brown //2) - a + 2 
+        if a*b == (brown+yellow): # a*b와 갈색,노란색 격자를 더한 값이 같으면 종료
             answer=[b,a]
             return answer
         a += 1
