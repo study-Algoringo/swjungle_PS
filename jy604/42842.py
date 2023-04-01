f5c30b34eda2936fdf44d1903e16a3ffd14c5a35
jy604 <126448821+jy604@users.noreply.github.com>
2023-04-01 11:29:07 +0900

    DAY 01 study 문제 해결
    스터디 후 문제 해결한 코드로 수정함

--- jy604/42842.py
+++ jy604/42842.py
@@ -1,12 +1,12 @@
 def solution(brown, yellow):
     answer = []
-    # brown 높이 3부터 시작 높이가 1 늘어날때마다 2의 제곱만큼 세로 길이 증가
-    # brown은 항상 짝수
-    # yellow는 brown에 둘러싸여있음 > brown의 모서리 4개를 제외하고 b의 개수의 제곱 = y의 개수
-    # 카펫이 (3,3) (4,4)일경우 예외
-    # 모서리를 뺀 브라운의 개수 /2 
-    a = int((brown - 4) /2)
-    for i in range(1, int((a+1)/2+1)):
-        if i*(a-i) == yellow:
-
+    # brown * yellow = 전체 넓이
+    total = brown + yellow
+    # 높이를 3부터 시작, 오름차순으로 증가
+    for h in range(3, total + 1):
+        if total % h == 0:
+            w = total / h
+            if (w - 2) * (h - 2) == yellow:
+                answer = [w, h]
+                break
     return answer
