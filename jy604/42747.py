1920bab030893892a4e625d07cc135d497d8d48f
jy604 <126448821+jy604@users.noreply.github.com>
2023-04-03 10:22:55 +0900

    [PGS] H-Index / Level2 / 42747 / 박정영
    런타임 에러 수정

--- jy604/42747.py
+++ jy604/42747.py
@@ -3,8 +3,6 @@ def solution(citations):
     cnt = len(citations) # 전체 논문 수
     answer = 0
     for i in range(cnt):
-        if citations[i] >= cnt - i
+        if citations[i] >= cnt - i:
             return cnt - i
     return answer
-
-# 반례 : h값이 배열에 들어있지 않는 경우
