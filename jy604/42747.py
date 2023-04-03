32d2e01f0dc136be9bad347c9887727551264b61
jy604 <126448821+jy604@users.noreply.github.com>
2023-04-03 09:36:21 +0900

    [PGS] H-Index / Level2 / 42747 / 박정영
    https://school.programmers.co.kr/learn/courses/30/lessons/42747

---
+++ jy604/42747.py
@@ -0,0 +1,10 @@
+def solution(citations):
+    citations.sort() #0, 1, 3, 5, 6
+    cnt = len(citations) # 전체 논문 수
+    answer = 0
+    for i in range(cnt):
+        if citations[i] >= cnt - i
+            return cnt - i
+    return answer
+
+# 반례 : h값이 배열에 들어있지 않는 경우
