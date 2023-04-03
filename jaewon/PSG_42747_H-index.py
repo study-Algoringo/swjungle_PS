c31fb719e8bcec21a23a3d7c7e133197f1c10a5d
jaewon4 <kimjaewon0901@gmail.com>
2023-04-03 11:18:32 +0900

    [PGS] H-Index / Level2 / 42747-update
    https://school.programmers.co.kr/learn/courses/30/lessons/42747

--- jaewon/PSG_42747_H-index.py
+++ jaewon/PSG_42747_H-index.py
@@ -1,14 +1,15 @@
 def solution(citations):
-    citations.sort()
-    cnt = 1
-    for i in range(len(citations)):
-        for j in range(i, len(citations)):
-            if citations[i] < citations[j]:
-                h = citations[i]
-                tmp = citations.index(citations[i])
-                x, y = h, len(citations) - h
-
+    # citations : 논문이 인용된 횟수가 들어있는 배열
     answer = 0
+    citations.sort(reverse=True)
+    for i in range(len(citations)):
+        # 여기서 i + 1이 h인덱스가 된다.
+        # [6, 5, 3, 1, 0] -> 정렬한 배열
+        # [1, 2, 3, 4, 5] -> 인덱스 번호
+        # 아래 조건문으로 비교하여 h인덱스 갱신
+        # 정의 : 인덱스가 3이라면 3번이상 인용된 논문의 개수가 3편 이상이고 나머지 논문이 3평 이하로 인용되면 h인덱스가 된다.
+        if citations[i] >= i + 1:
+            answer = i + 1
     return answer
 
 citations = [3, 0, 6, 1, 5]
