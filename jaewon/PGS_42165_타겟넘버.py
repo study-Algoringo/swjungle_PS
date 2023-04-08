1558c732a48fd803517ebbcf546aaec03ae9dafb
jaewon4 <kimjaewon0901@gmail.com>
2023-04-08 11:01:32 +0900

    [PGS] 타겟넘버 / Level2 / 43165-update
    https://school.programmers.co.kr/learn/courses/30/lessons/43165

--- jaewon/PGS_42165_타겟넘버.py
+++ jaewon/PGS_42165_타겟넘버.py
@@ -1,22 +1,15 @@
-from itertools import permutations
-
 def solution(numbers, target):
     answer = 0
-    tmp = []
-    cnt = 0
-    for i in range(len(numbers)):
-        tmp.append(str(numbers[i]))
-        tmp.append(str(-numbers[i]))
-
-    tmp = list(set(permutations(tmp, len(numbers))))
-    
-    for i in range(len(tmp)):
-        a = list(map(int, tmp[i]))
-        if sum(a) == target:
-            cnt += 1
-    
-    answer = cnt
-
+    cal = [0]       # 계산해준 숫자 리스트
+    for n in numbers:   
+        temp = []    # 계산해준 숫자 리스트를 갱신해줄 리스트
+        for i in cal: # # 계산해준 숫자 리스트에 numbers의 있는 숫자를 순서대로 계산
+            temp.append(i + n)  
+            temp.append(i - n)
+        cal = temp  # 계산해준 숫자 리스트를 갱신
+    for j in cal:
+        if j == target:
+            answer += 1
     return answer
 
 # solution([1, 1, 1, 1, 1], 3)
