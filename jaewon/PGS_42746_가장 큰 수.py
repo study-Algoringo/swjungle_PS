759dff2087b6d266d82c2a66f24df95844050e94
jaewon4 <kimjaewon0901@gmail.com>
2023-04-11 11:47:14 +0900

    [PGS] 가장 큰 수 / Level2 / 42746-update
    https://school.programmers.co.kr/learn/courses/30/lessons/42746

--- jaewon/PGS_42746_가장 큰 수.py
+++ jaewon/PGS_42746_가장 큰 수.py
@@ -1,13 +1,10 @@
 def solution(numbers):
-    tmp = list(''.join(map(str, numbers)))
-    tmp = list(map(int, tmp))
-    tmp.sort(reverse=True)
-    answer = ''.join(map(str, tmp))
-    return answer
+    numbers = list(map(str, numbers))
+    # 람다 사용법 숙지하기 문자열 정렬!!!!
+    numbers.sort(key=lambda x: x * 3, reverse=True)
+    return str(int(''.join(numbers)))
 
 print(solution([3, 30, 34, 5, 9]))
-
-
 
 # 풀었는데 시간초과....................................
 # from itertools import permutations
@@ -21,3 +18,6 @@ print(solution([3, 30, 34, 5, 9]))
 #     answer = str(max(res))
 
 #     return answer
+
+# 참고사이트
+# https://jokerldg.github.io/algorithm/2021/05/06/most-big-number.html
