bc890c02979b6a2ce7b060ac019aab952ccd73a7
ssue <wldmx@naver.com>
2023-04-15 11:01:35 +0900

    내용 수정

--- suhyun/42839.py
+++ suhyun/42839.py
@@ -22,13 +22,13 @@ def solution(numbers):
     return answer
 
 # 다른 사람의 풀이
-def solution(n):
-    a = set()
-    for i in range(len(n)):
-        a |= set(map(int, map("".join, permutations(list(n), i + 1))))
-    a -= set(range(0, 2))
-    for i in range(2, int(max(a) ** 0.5) + 1):
-        a -= set(range(i * 2, max(a) + 1, i))
-    return len(a)
+# def solution(n):
+#     a = set()
+#     for i in range(len(n)):
+#         a |= set(map(int, map("".join, permutations(list(n), i + 1))))
+#     a -= set(range(0, 2))
+#     for i in range(2, int(max(a) ** 0.5) + 1):
+#         a -= set(range(i * 2, max(a) + 1, i))
+#     return len(a)
 
 print(solution("17"))
