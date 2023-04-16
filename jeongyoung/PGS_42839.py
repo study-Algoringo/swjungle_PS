fec060458ff88f3aa20a6874fb488b85a2449708
jy604 <126448821+jy604@users.noreply.github.com>
2023-04-16 16:34:37 +0900

    [PGS] 소수 찾기 / Level2 / 42839 / 박정
    https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=python3
    4/15 문제 답안 참고

---
+++ jeongyoung/PGS_42839.py
@@ -0,0 +1,25 @@
+from itertools import permutations
+# 소수를 판별하는 함수 만들기
+def sosu(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def solution(numbers):
+    answer = 0
+    num = []
+    result = []
+    for i in range(1, len(numbers) + 1):
+        # for i in permutations(numbers, i):
+        num.extend(permutations(numbers, i))
+        result = [int(''.join(i)) for i in num]
+
+    for i in set(result):
+        if sosu(i):
+            answer += 1
+
+    return answer
