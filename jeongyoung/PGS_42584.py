4590d8cdfe4bbb4afe18eede244fb6790f13cfc6
jy604 <126448821+jy604@users.noreply.github.com>
2023-04-17 09:48:40 +0900

    [PGS] 주식가격 / Level2 / 42584 / 박정영
    https://school.programmers.co.kr/learn/courses/30/lessons/42584

---
+++ jeongyoung/PGS_42584.py
@@ -0,0 +1,11 @@
+#브루트포스
+def solution(prices):
+    answer = [0] * len(prices)
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
+                break
+    return answer
