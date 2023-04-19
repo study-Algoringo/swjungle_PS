912560103384e0f6ca44932ce82d69492586be60
jy604 <126448821+jy604@users.noreply.github.com>
2023-04-19 20:53:10 +0900

    [PGS] 위장 / Level2 / 42578 / 박정영-update

--- jeongyoung/PGS_42578.py
+++ jeongyoung/PGS_42578.py
@@ -1,7 +1,10 @@
 def solution(clothes):
-    answer = 1
-    # 딕셔너리 쓰기
+    # 딕셔너리 사용
+    # headgear = [yellow_hat, green_turban]
+    # eyegear = [blue_sunglasses]
+    # 안입은 것, 1번 입은 것, 2번 입은 것 = 3 * 2 - 모든 안 입은 경우 제외(1) = 5
     clothe_table = {}
+    answer = 1
     for i in clothes:
         key = i[1]
         value = i[0]
@@ -10,5 +13,5 @@ def solution(clothes):
         else:
             clothe_table[key] = [value]
     for key in clothe_table.values():
-
-
+        answer *= len(key) + 1
+    return answer - 1
