abfb5dcfb5ca6bcfdfd631ec29fd8ebe6f871c45
suhyun ji <wldmx3@gmail.com>
2023-04-22 10:04:22 +0900

    [PGS] 더 맵게 / Level2 / 42626
    https://school.programmers.co.kr/learn/courses/30/lessons/42626

---
+++ suhyun/42626.py
@@ -0,0 +1,21 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    result = scoville
+    length = len(scoville)
+    heapq.heapify(result)
+    
+    if sum(scoville) == 0:
+        return -1
+    
+    while result[0] < K:
+        if len(result) == 1:
+            return -1
+        
+        answer+=1
+        temp = heapq.heappop(result) + heapq.heappop(result) * 2
+        heapq.heappush(result,temp)
+
+    return answer
+
+print(solution([0,0,0,0,0],7))
