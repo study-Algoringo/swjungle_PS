95be91a3650a4baa20a374e9cddf925b5a483e69
jaewon4 <kimjaewon0901@gmail.com>
2023-04-22 11:17:54 +0900

    [PGS] 더 맵게 / Level2 / 42626
    https://school.programmers.co.kr/learn/courses/30/lessons/42626

--- jaewon/푸는중/PGS_42626_더 맵게.py
+++ jaewon/푸는중/PGS_42626_더 맵게.py
@@ -1,22 +1,25 @@
-from collections import deque
+import heapq
 
 def solution(scoville, K):
-    scoville.sort()
-    que = deque(scoville)
-    cnt = 0            
+    answer = 0
+    heapq.heapify(scoville)
 
-    while que:
-        n = que.popleft()
-        m = que.popleft()
-        cal = n + 2 * m
-        cnt += 1
-        
-        que.appendleft(cal)
-        
-        if min(que) >= K:
-            break
+    while len(scoville) >= 2:
+        # 가장 작은 수와 다음으로 작은 수 뽑기
+        smallest_val = heapq.heappop(scoville)
+        second_smallest_val = heapq.heappop(scoville)
+        # 가장 작은 수가 K보다 크다면 return
+        if smallest_val >= K:
+            return answer
 
-    answer = cnt
-    return answer
+        # 새로운 음식을 만든 후, 스코빌 지수 업데이트
+        new_val = smallest_val + (second_smallest_val * 2)
+        heapq.heappush(scoville, new_val)
+        answer += 1
 
-solution([1, 2, 3, 9, 10, 12], 7)
+    if heapq.heappop(scoville) < K:
+        return -1
+    else:
+        return answer
+
+print(solution([6,2],7))
