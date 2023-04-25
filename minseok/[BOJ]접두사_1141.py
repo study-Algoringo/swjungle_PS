2cf9c3959c68873f2e8ba99103f4ff6fe716b782
ms827 <min960827@gmail.com>
2023-04-25 11:34:16 +0900

    [BOJ] 접두사 / silver2 / 1141

--- minseok/[BOJ]접두사_1141.py
+++ minseok/[BOJ]접두사_1141.py
@@ -11,10 +11,9 @@ cnt = 0
 for i in range(n):
     for j in range(i+1,n):
         if words[j][:len(words[i])] == words[i]:
-            print(1,words[i],words[j])
             break
     else:
-        print(2,words[i],words[j])
         cnt += 1
 
 print(cnt)
+
