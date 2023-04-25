85420a057d31b8780ae330c706be6f6803307569
suhyun ji <wldmx3@gmail.com>
2023-04-25 12:42:25 +0900

    [BOJ] 접두사 / 실버1 / 1141 / 지수현
    https://www.acmicpc.net/problem/1141-re

--- suhyun/1141.py
+++ suhyun/1141.py
@@ -1,29 +1,26 @@
 import sys
-from itertools import combinations
 
-N = int(sys.stdin.readline())
-datas = [ sys.stdin.readline().rstrip() for _ in range(N)]
-datas.sort(key=lambda x : len(x))
+n = int(sys.stdin.readline())
+words = [(sys.stdin.readline()).rstrip() for _ in range(n)]
 
-temp = {}
-max_value = 0
-for data in datas:
-    if data not in temp:
-        temp[data] = []
+# 다른단어의 접두사가 되는 단어는 항상 다른단어보다 크기가 작거나 같다.
+# 따라서 문자열의 길이가 짧은 순서대로 정렬을 하고, 자기 위치보다 뒤에있는 단어와만 비교한다.
+words.sort(key=len)
+res = 0
+result = []
+# 반복문을 통해 단어를 확인
+for i in range(n):
+    flag = False
+    # 현재 단어보다 길이가 긴 단어를 확인
+    for j in range(i + 1, n):
+        # 현재 단어가 접두사인지 확인
+        if words[i] == words[j][0:len(words[i])]:
+            flag = True
+            result.append(words[i])
+            break
     
-    for j in range(N):
-        if data == datas[j][:len(data)]:
-            temp[data].append(datas[j])
-
-for key in temp.keys():
-    result=temp[key]
-    answer = 1
-    for i in range(N):
-        if datas[i] in result:
-            continue
-        
-        result.extend(temp[datas[i]])
-        answer+=1
-    max_value = max(max_value,answer)
-print(max_value)
-        
+    # 현재 단어가 접두사가 아니라면 res 카운트
+    if not flag:
+        res += 1
+print(result)
+print(res)
