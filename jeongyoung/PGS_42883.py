7b863d94741ca352f51757b87891b0ebabac1ad4
jy604 <126448821+jy604@users.noreply.github.com>
2023-04-26 04:44:25 +0900

    [PGS] 큰 수 만들기 / Level2 / 42883 / 박정영-update
    https://school.programmers.co.kr/learn/courses/30/lessons/42883
    시간 내 풀이 : ❌ 참고해서 다시 품

--- jeongyoung/PGS_42883.py
+++ jeongyoung/PGS_42883.py
@@ -1,20 +1,32 @@
+# 참고한 풀이
 def solution(number, k):
-    n = list(number)
-    answer = ''
-    i = 0
-    while k > 0:
-        start = n[i]
-        end = n[i+1]
-        if start != end:
-            if (start < end):
-                n.remove(start)
-                k -= 1
-            else:
-                n.remove(end)
-                k -= 1
-        else:
-            i += 1
+    answer = [] # 스택 활용
+    for n in number:
+        while k > 0 and answer and answer[-1] < n:
+            answer.pop()
+            k -= 1
+        answer.append(n)
 
-    return ''.join(n)
+    return ''.join(answer[:len(answer) - k])
+
+# 실패한 풀이
+# def solution(number, k):
+#     n = list(number)
+#     answer = ''
+#     i = 0
+#     while k > 0:
+#         start = n[i]
+#         end = n[i+1]
+#         if start != end:
+#             if (start < end):
+#                 n.remove(start)
+#                 k -= 1
+#             else:
+#                 n.remove(end)
+#                 k -= 1
+#         else:
+#             i += 1
+# 
+#     return ''.join(n)
 
 # 마지막 테스트 케이스 4177252841 결과값: 772841 기댓값: 775841
