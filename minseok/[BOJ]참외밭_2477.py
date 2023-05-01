f63cdd9def935f0d328c3cd921c6fb2db05a3731
ms827 <min960827@gmail.com>
2023-05-01 11:05:45 +0900

    update [BOJ] 참외밭 / Silver 2 / 2477

--- minseok/[BOJ]참외밭_2477.py
+++ minseok/[BOJ]참외밭_2477.py
@@ -4,38 +4,36 @@ input = sys.stdin.readline
 n = int(input())
 
 lst = []
-maxw = 0
-maxl = 0
-minw = int(1e9)
-minl = int(1e9)
+bigw = 0
+bigl = 0
+smallw = 0
+smalll = 0
 
 for i in range(6):
     lst.append(list(map(int,input().split())))
     if lst[i][0] == 1 or lst[i][0] == 2:
-        maxw = max(maxw,lst[i][1])
+        bigw = max(bigw,lst[i][1])
     if lst[i][0] == 3 or lst[i][0] == 4:
-        maxl = max(maxl,lst[i][1])
+        bigw = max(bigw,lst[i][1])
 
 for j in range(6):
     if j!=0 and j!=5:
         if (lst[j-1][0] == 1 and lst[j+1][0] == 1) or (lst[j-1][0] == 2 and lst[j+1][0] == 2):
-            minw = min(minw,lst[j][1])
+            smalll = lst[j][1]
         if (lst[j-1][0] == 3 and lst[j+1][0] == 3) or (lst[j-1][0] == 4 and lst[j+1][0] == 4):
-            minl = min(minl,lst[j][1])
+            smallw = lst[j][1]
     if j==0:
         if (lst[-1][0] == 1 and lst[j+1][0] == 1) or (lst[-1][0] == 2 and lst[j+1][0] == 2):
-            minw = min(minw,lst[j][1])
+            smalll = lst[j][1]
         if (lst[-1][0] == 3 and lst[j+1][0] == 3) or (lst[-1][0] == 4 and lst[j+1][0] == 4):
-            minl = min(minl,lst[j][1])
+            smallw = lst[j][1]  
     if j==5:
-        if (lst[-1][0] == 1 and lst[0][0] == 1) or (lst[-1][0] == 2 and lst[0][0] == 2):
-            # minw = min(minw,lst[j][1])
-            minw = lst[j][1]
-        if (lst[-1][0] == 3 and lst[0][0] == 3) or (lst[-1][0] == 4 and lst[0][0] == 4):
-            # minl = min(minl,lst[j][1])
-            minl = lst[j][1]
+        if (lst[j-1][0] == 1 and lst[0][0] == 1) or (lst[j-1][0] == 2 and lst[0][0] == 2):
+            smalll = lst[j][1]
+        if (lst[j-1][0] == 3 and lst[0][0] == 3) or (lst[j-1][0] == 4 and lst[0][0] == 4):
+            smallw = lst[j][1]
 
+print((bigw*bigl-smallw*smalll)*n)
 
-print((maxw*maxl-minw*minl)*n)
 
 
