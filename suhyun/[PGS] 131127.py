cbdc05e8cc182b7382412b687222884cdb0bad36
suhyun ji <wldmx3@gmail.com>
2023-05-04 11:04:25 +0900

    [PGS] 할인 행사 / level2 / 131127
    https://school.programmers.co.kr/learn/courses/30/lessons/131127-re

--- suhyun/[PGS] 131127.py
+++ suhyun/[PGS] 131127.py
@@ -1,3 +1,4 @@
+import time
 def solution(want, number, discount):
     answer = 0
     length = len(discount) - sum(number) + 1
@@ -21,5 +22,26 @@ def solution(want, number, discount):
             answer+=1
 
     return answer
+
+# K-상수님의 생각
+# def solution(want, number, discount):
+#     answer = 0
+#     cnt = 0
+#     n = len(want)
+#     for i in range(len(discount)):
+#         if discount[i] in want:
+#             j = want.index(discount[i])
+#             number[j] = number[j] -1    
+
+#         if i > 9:
+#             if discount[i-10] in want:
+#                 number[want.index(discount[i-10])] += 1
+
+#         for num in number:
+#             if num != 0:
+#                 break   
+#         else: answer += 1
+
+#     return answer
 
 print(solution(["banana", "apple", "rice", "pork", "pot"],[3, 2, 2, 2, 1],["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"]))
