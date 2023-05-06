f3464794673517fca6d22f14219b2cd289aef44b
Jeon-Yoojin <jeonryu@sookmyung.ac.kr>
2023-05-06 10:27:32 +0900

    [BOJ] 나이트의 이동 / 실버1 / 7562
    https://www.acmicpc.net/problem/7562

--- yoojin/PGS_43105.py
+++ yoojin/PGS_43105.py
@@ -5,8 +5,10 @@ def solution(triangle):
     dp[0][0] = triangle[0][0]
     for i in range(1, len(triangle)):
         for j in range(0, len(triangle[i])):
-            if j == 0 or j == len(triangle[i]) - 1:
+            if j == 0:
                 dp[i][j] = dp[i-1][j] + triangle[i][j]
+            elif j == len(triangle[i]) - 1:
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
             else:
                 dp[i][j] = max(dp[i-1][j-1] + triangle[i][j], dp[i-1][j] + triangle[i][j])
     answer = max(dp[len(triangle)-1])
