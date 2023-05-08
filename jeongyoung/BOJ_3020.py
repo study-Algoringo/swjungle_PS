83e888bc899bdd596beb2cc7fa145ee751639808
jy604 <126448821+jy604@users.noreply.github.com>
2023-05-09 03:58:02 +0900

    [BOJ] 개똥벌레 / Gold 5 / 3020 / 김상수
    https://www.acmicpc.net/problem/3020

---
+++ jeongyoung/BOJ_3020.py
@@ -0,0 +1,29 @@
+n, h = map(int, input().split())
+
+seok = [] # 석순
+jong = [] # 종유석
+
+# 첫번째가 석순 = 홀수
+for i in range(n):
+    if i % 2 == 0: # 짝수 = 종유석
+        jong.append(int(input()))
+    else:
+        seok.append(int(input()))
+
+seok.sort()
+jong.sort()
+
+
+def search(arr, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+
+        if arr[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return start
+
+
+for i in range(1, h + 1):
+    
