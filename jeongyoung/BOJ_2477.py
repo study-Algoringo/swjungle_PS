97ce014c51a3c623db7968f82f6567b3f9f0cb8c
jy604 <126448821+jy604@users.noreply.github.com>
2023-05-08 16:34:09 +0900

    [BOJ] 참외밭 / Silver 2 / 2477 / 박정영 -update
    [BOJ] 참외밭 / Silver 2 / 2477 / 박정영 -update

--- jeongyoung/BOJ_2477.py
+++ jeongyoung/BOJ_2477.py
@@ -5,6 +5,7 @@ a = [list(map(int, input().split())) for _ in range(6)]
 w = []
 h = []
 result = 0
+s_a = []
 
 for i in range(6):
     if a[i][0] == 1 or a[i][0] == 2:
@@ -12,7 +13,11 @@ for i in range(6):
     else:
         h.append(a[i][1])
     # print(w, h)
-w.sort()
-h.sort()
 # print(abs(((w[0] * h[0]) - (w[2] * h[2]))* k))
-print((max(w) * max(h) - min(w) * min(h))*k)
+
+#s_a의 길이
+for i in range(6):
+    if a[i][0] == a[(i+2)%6][0]:
+        s_a.append(a[(i+1)%6][1])
+
+print(((max(w) * max(h)) - (s_a[0]*s_a[1]))*k)
