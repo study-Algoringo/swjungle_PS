b30aff594821e52365e4a1c84007ed465eba8bef
Jeon-Yoojin <jeonryu@sookmyung.ac.kr>
2023-05-11 10:30:04 +0900

    [PGS] 귤 고르기 / level2 / 138476
    https://school.programmers.co.kr/learn/courses/30/lessons/138476

--- yoojin/PGS_138476.py
+++ yoojin/PGS_138476.py
@@ -1,14 +1,13 @@
+from collections import Counter
+
 def solution(k, tangerine):
     answer = 0
-    count = [0] * 100000
-    for tan in tangerine:
-        count[tan] += 1
-        
-    while k > 0:
-        mx = max(count)
-        if mx == 0:
-            return 0
-        k = k - mx
-        count[count.index(mx)] = 0
+    count = Counter(tangerine).most_common()
+    
+    for i in range(len(count)):
+        k -= count[i][1]
         answer += 1
+        if k <= 0:
+            break
+        
     return answer
