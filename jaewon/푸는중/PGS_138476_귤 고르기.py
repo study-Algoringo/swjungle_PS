79a63e30cf3ef0803ffc7c20117ddb528c1b63de
jaewon4 <kimjaewon0901@gmail.com>
2023-05-11 10:14:03 +0900

    [PGS] 귤 고르기 / Level 2 / 138476

---
+++ jaewon/푸는중/PGS_138476_귤 고르기.py
@@ -0,0 +1,22 @@
+def solution(k, tangerine):
+    answer = 0
+    num_list = list(set(tangerine))
+    cnt_list = [0] * len(num_list)
+    for i in range(len(cnt_list)):
+        cnt_list[i] = tangerine.count(num_list[i])
+    
+    res = 0
+    cnt = 0
+    for i in range(len(cnt_list)):
+        res += max(cnt_list)
+        cnt_list.remove(max(cnt_list))
+        cnt += 1
+        if res >= k:
+            answer = cnt
+            break
+
+    return answer
+
+arr = [1, 3, 2, 5, 4, 5, 2, 3]
+
+print(solution(6, arr))
