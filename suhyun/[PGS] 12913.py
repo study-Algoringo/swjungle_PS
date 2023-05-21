c73cde85077b89e1e9f8ed55742c3fcc536d361e
suhyun ji <wldmx3@gmail.com>
2023-05-21 14:19:34 +0900

    [PGS] 땅따먹기 / Level2 / 12913-re
    https://school.programmers.co.kr/learn/courses/30/lessons/12913

--- suhyun/[PGS] 12913.py
+++ suhyun/[PGS] 12913.py
@@ -1,22 +1,32 @@
-def solution(land):
-    answer = 0
-    dp = [[0,0,0,0] for _ in range(len(land))]
-    dp[0] = land[0]
+# def solution(land):
+#     answer = 0
+#     dp = [[0,0,0,0] for _ in range(len(land))]
+#     dp[0] = land[0]
 
+#     # 행 계산
+#     for i in range(1,len(land)):
+#         # 열 계산
+#         for j in range(4):
+#             if j == 0:
+#                 dp[i][j] = max(dp[i-1][1],dp[i-1][2],dp[i-1][3])
+#             elif j == 1:
+#                 dp[i][j] = max(dp[i-1][0],dp[i-1][2],dp[i-1][3])
+#             elif j == 2:
+#                 dp[i][j] = max(dp[i-1][0],dp[i-1][1],dp[i-1][3])
+#             else:
+#                 dp[i][j] = max(dp[i-1][0],dp[i-1][1],dp[i-1][2])
+#             dp[i][j] += land[i][j]
+
+#     return max(dp[-1])
+
+# 최적화
+def solution(land):
     # 행 계산
     for i in range(1,len(land)):
         # 열 계산
         for j in range(4):
-            if j == 0:
-                dp[i][j] = max(dp[i-1][1],dp[i-1][2],dp[i-1][3])
-            elif j == 1:
-                dp[i][j] = max(dp[i-1][0],dp[i-1][2],dp[i-1][3])
-            elif j == 2:
-                dp[i][j] = max(dp[i-1][0],dp[i-1][1],dp[i-1][3])
-            else:
-                dp[i][j] = max(dp[i-1][0],dp[i-1][1],dp[i-1][2])
-            dp[i][j] += land[i][j]
+            land[i][j]+=max(land[i-1][:j]+land[i-1][j+1:])
 
-    return max(dp[-1])
+    return max(land[-1])
 
 print(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]))
