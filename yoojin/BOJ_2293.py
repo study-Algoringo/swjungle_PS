0491cda779334f1287ca16bb2b39ed6128e1f71c
Jeon-Yoojin <jeonryu@sookmyung.ac.kr>
2023-07-18 21:00:52 +0900

    [BOJ] 정수 삼각형(S1), 동전 1(G5), 연속합 2(G5)

---
+++ yoojin/BOJ_2293.py
@@ -0,0 +1,16 @@
+import sys
+read = sys.stdin.readline
+
+n, k = map(int, read().split())
+coins = []
+dp = [0] * (k + 1)
+
+for _ in range(n):
+    coins.append(int(read()))
+
+dp[0] = 1
+for coin in coins:
+    for i in range(coin, k  + 1):
+        dp[i] += dp[i - coin]
+
+print(dp[k])
