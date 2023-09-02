856d503dc9d26301f5ee7d0f3641bf1f8ce18e1b
Jeon-Yoojin <jeonryu@sookmyung.ac.kr>
2023-09-02 23:57:17 +0900

    [BOJ] 부분수열의 합 / Silver2 / 1182

--- yoojin/BOJ_1182.py
+++ yoojin/BOJ_1182.py
@@ -1,3 +1,21 @@
 import sys
 read = sys.stdin.readline
 
+n, s = map(int, read().split())
+nums = list(map(int, read().split()))
+
+answer = 0
+def dfs(idx, sum):
+    global answer
+
+    if idx >= n:
+        return
+    
+    sum += nums[idx]
+    if sum == s:
+        answer += 1
+    dfs(idx + 1, sum)
+    dfs(idx + 1, sum - nums[idx])
+
+dfs(0, 0)
+print(answer)
