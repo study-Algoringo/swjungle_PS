fab909a39cf1ba0b63a819fea7c628b8d87345f0
Jeon-Yoojin <jeonryu@sookmyung.ac.kr>
2023-09-12 00:01:23 +0900

    [BOJ] 수열의 합 / Silver2 / 1024 / 푸는중

--- yoojin/BOJ_1024.py
+++ yoojin/BOJ_1024.py
@@ -3,11 +3,13 @@ read = sys.stdin.readline
 
 n, l = map(int, read().split())
 
-# 가운데 숫자를 기준으로 대칭 이루는 경우
 for length in range(l, 101):
     start = (2 * n - length * (length - 1)) / (2 * length)
 
     if start.is_integer() and start >= 0:
         start = int(start)
         sequence = list(range(start, start + length))
-# 좌우 대칭이 아닌 경우
+        print(*sequence)
+        break
+else:
+    print(-1)
