83c3097d9b7531554a0d59175977aa9861b9940a
Jeon-Yoojin <jeonryu@sookmyung.ac.kr>
2023-09-15 23:59:06 +0900

    [BOJ] 경로 찾기 / Silver1 / 11403 / 푸는중

--- yoojin/BOJ_11403.py
+++ yoojin/BOJ_11403.py
@@ -4,4 +4,10 @@ read = sys.stdin.readline
 n = int(read())
 graph = [list(map(int, read().split())) for _ in range(n)]
 
-
+# 시작점 i에서 시작했을 때, 도착할 수 있는 지점을 모두 표시함 (i, j)
+def dfs(node, visited):
+    visited[node] = True
+    # 갈 수 있는 모든 정점을 dfs로 탐색
+    for i in graph[node]:
+        if i == 1:
+            dfs(i, visited)
