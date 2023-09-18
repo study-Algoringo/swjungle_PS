9f17832940af7034312dfe77fd21272aee644edb
Jeon-Yoojin <jeonryu@sookmyung.ac.kr>
2023-09-18 23:41:16 +0900

    [BOJ] 용액 / Gold5 / 2467

--- yoojin/BOJ_2467.py
+++ yoojin/BOJ_2467.py
@@ -3,5 +3,25 @@ read = sys.stdin.readline
 
 n = int(read())
 # 합이 0에 가장 가까운 두 용액의 조합
-nums = map(int, read().split())
+nums = list(map(int, read().split()))
 
+temp = 10000000001
+left = 0; right = n - 1
+while left < right:
+    sum = nums[left] + nums[right]
+    if abs(sum) <= temp:
+        temp = abs(sum)
+        answer = (nums[left], nums[right])
+    if sum > 0:
+        right -= 1
+    else:
+        left += 1
+
+# 음수 두개의 합 중 가장 큰 값
+if temp > abs(nums[0] + nums[1]):
+    answer = (nums[0], nums[1])
+if temp > abs(nums[-1] + nums[-2]):
+    answer = (nums[-2], nums[-1])
+
+for i in answer:
+    print(i, end=' ')
