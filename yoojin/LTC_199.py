05a247b0bd25b2948742d0d35acd8d07e96e20a0
Jeon-Yoojin <jeonryu@sookmyung.ac.kr>
2023-11-18 01:44:07 +0900

    [LTC] Binary Tree Right Side View / Medium / 199

--- yoojin/LTC_199.py
+++ yoojin/LTC_199.py
@@ -1,9 +1,25 @@
+from collections import deque
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        
+        result = []
+        queue = deque([root])
+
+        while queue:
+            rightMost = None
+            for _ in range(len(queue)):
+                current = queue.popleft()
+                if current:
+                    rightMost = current
+                    queue.append(current.left)
+                    queue.append(current.right)
+            if rightMost:
+                result.append(rightMost.val)
+        return result
